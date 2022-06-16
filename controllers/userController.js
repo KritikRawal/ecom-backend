6338fb6d0c518b2c568f610834776458520400bb
summit999 <summitnakarmi109@gmail.com>
2022-06-16 13:37:12 +0545

    update controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,7 +2,56 @@ import e from 'express'
 import asyncHandler from 'express-async-handler'
 import User from '../models/userModel.js'
 import generateToken from '../utils/generateToken.js'
+//@desc    Auth user & get token
+//@route   POST /api/users/login
+//@access  Public
+const authUser = asyncHandler(async (req, resp) => {
+    const { email, password } = req.body
+    const user = await User.findOne({ email })
 
+    if (user && (await user.matchPassword(password))) {
+        resp.json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            isAdmin: user.isAdmin,
+            token: generateToken(user._id)
+        })
+    } else {
+        resp.status(401)
+        throw new Error('Invalid email or password')
+    }
+})
+//@desc    Register a new user
+//@route   POST /api/users
+//@access  Public
+const registerUser = asyncHandler(async (req, resp) => {
+    const { name, email, password } = req.body
+    const userExists = await User.findOne({ email })
+    if (userExists) {
+        resp.status(400)
+        throw new Error('User already exists')
+    }
+
+    const user = await User.create({
+        name,
+        email,
+        password
+    })
+
+    if (user) {
+        resp.status(201).json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            isAdmin: user.isAdmin,
+            token: generateToken(user._id)
+        })
+    } else {
+        resp.status(400)
+        throw new Error('Invalid user data')
+    }
+})
 //@desc    get user profile
 //@route   GET /api/users/profile
 //@access  Private
